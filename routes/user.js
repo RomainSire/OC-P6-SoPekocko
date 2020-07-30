cb40003ea11ee8f4d7858096f1e5cb7844755ac5
Romain <romain.sire@gmx.com>
2020-07-30 18:53:20 +0200

    Validation des données utilisateurs lors du login et signup

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+const userCtrl = require('../controllers/user');
+const validator = require('../middlewares/validator');
+
+router.post(
+    '/signup',
+    validator.userValidationRules(),
+    validator.validate,
+    userCtrl.signup
+);
+router.post(
+    '/login',
+    validator.userValidationRules(),
+    validator.validate,
+    userCtrl.login
+);
 
 module.exports = router;
