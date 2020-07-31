dfd0165bafb5087e7d0d13678cda9affb7157b28
Romain <romain.sire@gmx.com>
2020-07-31 17:01:00 +0200

    Sécurité : meilleure configuration du CORS

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ const app = express();
 /**
  * MIDDLEWARES
  */
-// cors
+// Configuration cors
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', process.env.AUTHORIZED_ORIGIN);
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
